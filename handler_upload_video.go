852e4bbedb3cae6057c174fd1271fc11ec33c42e
Demilade Omitiran <omitirand@gmail.com>
2025-07-08 11:12:07 +0100

    Create handler for video upload

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,116 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	const maxMemory = 1 << 30
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not your video", err)
+		return
+	}
+
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	contentTypeHeader := header.Header.Get("Content-Type")
+
+	mediaType, _, err := mime.ParseMediaType(contentTypeHeader)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Media Type", err)
+		return
+	}
+
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Invalid Media Type", err)
+		return
+	}
+
+	tempFile, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create temp file", err)
+		return
+	}
+	defer os.Remove(tempFile.Name())
+
+	defer tempFile.Close()
+
+	if _, err := io.Copy(tempFile, file); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't save the file", err)
+		return
+	}
+
+	if _, err = tempFile.Seek(0, io.SeekStart); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read file from beginning", err)
+		return
+	}
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	s3Key := fmt.Sprintf("%x.mp4", key)
+
+	s3PutObjectInputParam := s3.PutObjectInput{
+		Bucket:      &cfg.s3Bucket,
+		Key:         &s3Key,
+		Body:        tempFile,
+		ContentType: &mediaType,
+	}
+
+	if _, err = cfg.s3Client.PutObject(r.Context(), &s3PutObjectInputParam); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to upload file to amazon", err)
+		return
+	}
+
+	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, s3Key)
+
+	videoMetadata.VideoURL = &videoURL
+
+	if err = cfg.db.UpdateVideo(videoMetadata); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update video URL", err)
+		return
+	}
+}
